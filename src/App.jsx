7aec4d8e728f5e2a1c1828de13b2fecb4aa4f92d
Kai Mitchell <kaimitchell@gmail.com>
2025-03-02 13:42:39 +1300

    Begin fabricating basic layout for set up display

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,40 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>
-        Hello There.
-      </h1>
-    </>
+    <div className="flex flex-col justify-between w-screen h-screen py-5 px-1">
+      <header>
+        <h1 className="text-3xl text-center underline">
+          INTERVAL
+        </h1>
+      </header>
+
+      <main className="text-center">
+        <div className="p-5 border border-black">
+          <p>On</p>
+          <h3>0s</h3>
+        </div>
+        <div className="p-5 border border-black">
+          <p>Off</p>
+          <h3>0s</h3>
+        </div>
+        <div className="p-5 border border-black">
+          <p>Rounds</p>
+          <h3>0</h3>
+        </div>
+        <div className="p-5 border border-black">
+          <p>Prep time</p>
+          <h3>0s</h3>
+        </div>
+        <div className="p-5 border border-black">
+          <h3 className="font-bold">EXERCISES</h3>
+        </div>
+      </main>
+
+      <footer className="self-center w-1/2 text-center">
+        <div className="p-2.5 mb-2.5 border border-black">clear</div>
+        <div className="p-2.5 border border-black">Set</div>
+      </footer>
+    </div>
   )
 }
 
